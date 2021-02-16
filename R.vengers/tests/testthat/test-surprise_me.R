d30b8b74ad32c87528b83163fe938c840269e640
RaineShen <rain.ya1213@gmail.com>
2021-02-15 17:45:56 -0800

    update surprise_me and unittest

--- R.vengers/tests/testthat/test-surprise_me.R
+++ R.vengers/tests/testthat/test-surprise_me.R
@@ -1,12 +1,12 @@
 test_that("correct surprise_me output", {
   
-  output <- surprise_me()
+  output <- surprise_me_new(2)
   expect_is(output, 'data.frame')
   expect_equal(ncol(output), 9)
-  expect_equal(nrow(output), 7)
+  expect_equal(nrow(output), 7*2)
   expect_is(output$Genre, 'character')
   expect_equal(colnames(output)[5] , 'Runtime (in minutes)')
-  expect_match(sample(output[[4]], 1), '*drama*', ignore.case = TRUE)
   expect_equal(nrow(output), length(unique(output$Title)))
+  expect_error(surprise_me_new(12))
   
 })
