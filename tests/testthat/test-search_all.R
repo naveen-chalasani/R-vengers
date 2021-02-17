ab26d1d4f6d3abe7913d36eb2e4efc78e396ecc0
naveen-chalasani <chalasani.naveen@outlook.com>
2021-02-16 18:05:32 -0800

    try to fix CI errors

--- tests/testthat/test-search_all.R
+++ tests/testthat/test-search_all.R
@@ -1,12 +1,12 @@
 test_that("correct search output", {
   
-  output <- search('queen', 'movie')
+  output <- search_all('queen', 'movie')
   expect_type(output, 'list')
   expect_true(is.data.frame(output))
   expect_equal(ncol(output), 9)
   expect_type(output$`IMDB Rating`, 'double')
   expect_equal(colnames(output)[5] , 'Runtime (in minutes)')
-  expect_equal(search('the queen'), search('the+queen'))
+  expect_equal(search_all('the queen'), search_all('the+queen'))
   expect_match(sample(output[[1]], 1), '*queen*', ignore.case = TRUE)
   expect_equal(nrow(output), length(unique(output$Title)))
   
